7af9c0d5e47d63bf6596b47428f976fcb66a921f
Ganya04 <ganyajanardhan04@gmail.com>
2024-06-21 15:27:49 -0500

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => (
   <footer className="footer">
     <a href="/portfolio" className="link footer__link">
-      Created By Abhiram B J
+      Created By Ganya Janardhan
     </a>
   </footer>
 );
