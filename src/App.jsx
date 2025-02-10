ee9fde4de3ffeda610db48cc5293d1a43a1cca90
GANYA JANARDHAN <ganyajanardhan04@gmail.com>
2025-02-10 15:55:01 -0600

    modified app.jsx router config

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 
 function App() {
-	return (
-		<div className="min-h-screen bg-light-primary text-light-text dark:bg-dark-primary dark:text-dark-text transition-colors duration-300">
-			<Routes>
-				{/* Main route */}
-				<Route path="/" element={<Main />} />
+  return (
+    <div className="min-h-screen bg-light-primary text-light-text dark:bg-dark-primary dark:text-dark-text transition-colors duration-300">
+      <Routes>
+        {/* Main route */}
+        <Route path="/portfolio/" element={<Main />} />
 
-				{/* Catch-all route for 404 errors */}
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</div>
-	);
+        {/* Catch-all route for 404 errors */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
