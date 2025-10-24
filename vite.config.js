a480f08a433abd5290971c887fec82cfb673f60e
Ganya Janardhan <janardhanganya@gmail.com>
2025-10-24 18:01:51 -0500

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    server: {
-      port: 3000,
-    },
-  };
-
-  if (command !== 'serve') {
-    config.base = '/portfolio/';
-  }
-
-  return config;
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/portfolio/',  
+  build: {
+    outDir: 'dist',
+  },
+})
